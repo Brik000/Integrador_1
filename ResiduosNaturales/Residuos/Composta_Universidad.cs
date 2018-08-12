8d8bc6f4ecd786615e0dd8414a492a6baa8648db
Brik000 <sdelcampo7@hotmail.com>
2018-08-12 15:43:49 -0500

    rep
    rep

--- ResiduosNaturales/Residuos/Composta_Universidad.cs
+++ ResiduosNaturales/Residuos/Composta_Universidad.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 namespace Residuos
 {
-    public class Composta_Universidad{
+    public class Composta_Universidad
+    {
 
         private int Capacidad;
         private int Cantidad_Actual;
         private bool Espacio_Disponible;
-        private ResiduosNaturales residuo_Actual;
+        private ResiduosNaturales[] residuo;
+        private int[] cantidadResiduo;
 
 
 
@@ -14,13 +16,67 @@ namespace Residuos
             Capacidad = 12345;
             Cantidad_Actual = 0;
             Espacio_Disponible = true;
+
+            residuo[0] = new ResiduosNaturales("organico", 1201, "pasto");
+            residuo[1] = new ResiduosNaturales("organico", 1202, "agua");
+            residuo[2] = new ResiduosNaturales("organico", 1203, "semillas");
+            residuo[3] = new ResiduosNaturales("organico", 1204, "papel");
+            residuo[4] = new ResiduosNaturales("organico", 1205, "carton");
+            residuo[5] = new ResiduosNaturales("organico", 1206, "huesos");
+            residuo[6] = new ResiduosNaturales("organico", 1207, "cascara huevos");
+            residuo[7] = new ResiduosNaturales("organico", 1208, "excremento animales");
+            residuo[8] = new ResiduosNaturales("organico", 1209, "flores");
+
+            cantidadResiduo = new int[residuo.Length];
+
+            for (int i = 0; i < cantidadResiduo.Length;i++){
+                int a = 0;
+                cantidadResiduo[i] = a;
+            }
+
         }
 
+        public int Elresiduoes(ResiduosNaturales este){
 
-        public void AgregarResiduos(int cantidadAgregar){
+            switch (este.Nombre){
+
+                case "pasto":
+                    return 0;
+
+
+                case "agua":
+                    return 1;
+
+                case "semillas":
+                    return 2;
+
+                case "papel":
+                    return 3;
+                   
+                case "carton":
+                    return 4;
+                  
+                case "huesos":
+                    return 5;
+                   
+                case "cascara huevos":
+                    return 6;
+
+                case "excremento animales":
+                    return 7;
+
+                case "flores":
+                    return 8;
+
+            }
+            return -1;
+        }
+
+        public void AgregarResiduos(int cantidadAgregar,ResiduosNaturales agregar){
             if(Capacidad>cantidadAgregar&&Espacio_Disponible){
                 Capacidad = Capacidad - cantidadAgregar;
                 Cantidad_Actual = Cantidad_Actual + cantidadAgregar;
+                cantidadResiduo[Elresiduoes(agregar)] += cantidadAgregar;
 
                 if(Capacidad==0){
                     Espacio_Disponible = false;
@@ -32,14 +88,14 @@ namespace Residuos
         }
 
 
-        public void RetirarResiduos(int cantidadRetirar){
+        public void RetirarResiduos(int cantidadRetirar,ResiduosNaturales retirar){
 
             if(Cantidad_Actual>0 && cantidadRetirar<Cantidad_Actual && cantidadRetirar<=Capacidad){
 
                 Cantidad_Actual = Cantidad_Actual - cantidadRetirar;
 
                 Capacidad = Capacidad + cantidadRetirar;
-
+                cantidadResiduo[Elresiduoes(retirar)] -= cantidadRetirar;
                 if(!Espacio_Disponible){
                     if(Capacidad>0){
                         Espacio_Disponible = true;
@@ -51,7 +107,7 @@ namespace Residuos
             }
         }
 
-
+public int 
 
 
     }
