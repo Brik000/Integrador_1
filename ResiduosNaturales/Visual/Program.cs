4a1974bf4654f18ebfc282f95a639613b4e1b4d8
juanfjaramillo98 <juanjaraa5@gmail.com>
2018-08-12 21:28:16 -0500

    solo falta el archivo txt

--- ResiduosNaturales/Visual/Program.cs
+++ ResiduosNaturales/Visual/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace Visual
 {
@@ -17,6 +19,59 @@ namespace Visual
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            lectura();
+            escritura();
         }
+
+        public static void lectura()
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("..\\..\\ejemplo.txt");
+
+                line = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                sr.Close();
+                //Console.ReadLine();
+                Thread.Sleep(4000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public static void escritura()
+        {
+            try
+            {
+
+                StreamWriter sw = new StreamWriter("..\\..\\ejemplo.txt", true);
+
+                sw.WriteLine("\n- Hola de nuevo mundo!!");
+
+                sw.WriteLine("- :D");
+
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
